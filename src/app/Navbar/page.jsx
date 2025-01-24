abd349ed5dbc4a8aefeef2a7d35de29368346b32
kausar ahammed <kausar.a.pro@gmail.com>
2025-01-24 17:58:35 +0600

    first commit

--- src/app/Navbar/page.jsx
+++ src/app/Navbar/page.jsx
@@ -34,17 +34,17 @@ const Navbar = () => {
         <div
           className={`${
             isMenuOpen ? "block" : "hidden"
-          } md:flex absolute md:static top-16 left-0 w-full md:w-auto bg-gradient-to-r from-blue-600 to-purple-600 md:bg-transparent md:flex-row flex flex-col items-center space-y-4 md:space-y-0 md:space-x-6 z-50`}
+          } md:flex absolute md:static top-16 left-0 w-full md:w-auto bg-gradient-to-r  md:bg-transparent md:flex-row flex flex-col items-center space-y-4 md:space-y-0 md:space-x-6 z-50`}
         >
           <Link
-            className="text-white font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
+            className="text-white font-semibold border py-2 px-4 rounded-md transition duration-300"
             href="/"
             onClick={() => setIsMenuOpen(false)}
           >
             Home
           </Link>
           <Link
-            className="text-white font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
+            className="text-white  border font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
             href="/Blogs"
             onClick={() => setIsMenuOpen(false)}
           >
@@ -53,14 +53,14 @@ const Navbar = () => {
           {isAuthenticated ? (
             <>
               <Link
-                className="text-white font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
+                className="text-white  border font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
                 href="/Profile"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Profile
               </Link>
               <LogoutLink
-                className="text-white font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
+                className="text-white  border font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Sign out
@@ -69,13 +69,13 @@ const Navbar = () => {
           ) : (
             <>
               <LoginLink
-                className="text-white font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
+                className="text-white  border font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Sign in
               </LoginLink>
               <RegisterLink
-                className="text-white font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
+                className="text-white  border font-semibold hover:bg-blue-700 py-2 px-4 rounded-md transition duration-300"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Sign Up
@@ -89,3 +89,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
